074af074309df6c153705c97ff09a481858538be
shariar-duke <shariarmahmud225@gmail.com>
2024-06-14 21:26:42 +0600

    rotation changes

--- src/components/KeyFrame.jsx
+++ src/components/KeyFrame.jsx
@@ -6,6 +6,7 @@ export default function KeyFrame() {
      animate={{
         // css gula react a use kortse jnno camel case a hosch 
         borderRadius:["20%","20%","50%","50%","20%"],
+        rotate:[0,90,180,270,0],
         transition:{duration:1}
      }}
     className='bg-blue-500 w-32 h-32 m-4'>
