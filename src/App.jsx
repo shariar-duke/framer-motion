db793ae77ce49222165fa5d4fabb587d32b2f4b5
shariar-duke <shariarmahmud225@gmail.com>
2024-06-14 14:20:33 +0600

    Basic of Animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 export default function App() {
   return (
     <div className=" border-black h-[100vh] ">
-      <p>This is all about framer motion</p>
+      <p>Basic of Animation</p>
     </div>
   );
 }
