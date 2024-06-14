a81f2965fb93ad150ac5a403e59f2c4b16a5c182
shariar-duke <shariarmahmud225@gmail.com>
2024-06-14 23:59:52 +0600

    two type of button motion

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,16 +1,34 @@
-import {motion} from 'framer-motion'
+import { motion } from "framer-motion";
 export default function Button() {
   return (
-    <div>
+    <div className="flex flex-col gap-8">
       <motion.button
-       className='bg-blue-500 text-white border-none p-2 text-[20px] rounded-md'
-       whileTap={{
-        scale:0.5
-      }}
-      whileHover={{
-        scale:1.1
-      }}
-      >Click Me</motion.button>
+        className="bg-blue-500 text-white border-none p-2 text-[20px] rounded-md mt-4"
+        whileTap={{
+          scale: 0.5,
+        }}
+        whileHover={{
+          scale: 1.4,
+        }}
+      >
+        Click Me
+      </motion.button>
+
+   
+        {" "}
+        <motion.button
+          className="bg-blue-500 text-white border-none p-2 text-[20px] rounded-md mt-4"
+          whileTap={{
+            scale: 0.5,
+          }}
+          whileHover={{
+            scale: 1.1,
+            transition:{yoyo:Infinity}
+          }}
+        >
+          Submit
+        </motion.button>
+     
     </div>
-  )
+  );
 }
