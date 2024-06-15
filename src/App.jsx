79df62deb6317c5495f8cb930a8c292f56fa80ab
shariar-duke <shariarmahmud225@gmail.com>
2024-06-15 06:34:46 +0600

    nested variant

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,17 @@ import Simple from "./components/Simple";
 import KeyFrame from "./components/KeyFrame";
 import Button from "./components/Button";
 import Text from "./components/Text";
+import TransitionType from "./components/TransitionType";
+import Variants from "./components/Variants";
 export default function App() {
   return (
-    <div className="h-[100vh] flex justify-center items-center bg-black" >  
+    <div className="h-[100vh] flex justify-center items-center">  
       {/* <Simple/> */}
       {/* <KeyFrame/> */}
       {/* <Button/> */}
-      <Text/>
+      {/* <Text/> */}
+      {/* <TransitionType/> */}
+      <Variants/>
     </div>
   );
 }
--- src/components/Variants.jsx
+++ src/components/Variants.jsx
@@ -1,6 +1,6 @@
 import { motion } from "framer-motion";
 
-// ekhne alad kor declare kore nise animation ta 
+// ekhne alad kor declare kore nise animation ta
 const shapeVariants = {
   initial: {
     opacity: 0,
@@ -18,16 +18,33 @@ const shapeVariants = {
   },
 };
 
+const childVariant = {
+  initial: {
+    sale: 1,
+    opacity:0
+  },
+
+  animate: {
+    opacity:1,
+    scale: 1.8,
+    transition: { duration: 3 },
+  },
+};
+
 export default function Variants() {
   return (
     <div>
       <motion.div
-      variants={shapeVariants} // first variant property er modhe oi animation tar nam dite hbe 
-      initial="initial" // tarpor initail er modhe dite hbe j shapeVariant er initial ta 
-      animate="animate"  // tarpor animate er modhe dite hbe shapeVariant er modher animate ta 
-      className="bg-green-700 w-[450px] h-[170px] rounded border m-4 flex justify-center items-center">
-
-        {/* <motion.div className="bg-red-500 w-20 h-20 rounded-full border m-2"></motion.div> */}
+        variants={shapeVariants} // first variant property er modhe oi animation tar nam dite hbe
+        initial="initial" // tarpor initail er modhe dite hbe j shapeVariant er initial ta
+        animate="animate" // tarpor animate er modhe dite hbe shapeVariant er modher animate ta
+        className="bg-green-700 w-[750px] h-[270px] rounded border m-4 flex justify-center items-center"
+      >
+        <motion.div 
+        variants={childVariant}
+        // initial="initial"
+        // animate="animate"
+        className="bg-red-500 w-20 h-20 rounded-full border m-2"></motion.div>
       </motion.div>
     </div>
   );
