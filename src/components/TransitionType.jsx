1a076400490d0efeaa3ec1497c2d81f2bae6e578
shariar-duke <shariarmahmud225@gmail.com>
2024-06-15 06:04:42 +0600

    spring animation on text

--- src/components/TransitionType.jsx
+++ src/components/TransitionType.jsx
@@ -2,7 +2,7 @@ import { motion } from "framer-motion";
 export default function TransitionType() {
   return (
     <>
-      <motion.div
+      {/* <motion.div
         initial={{ opacity: 0, x: "-100vh" }}
         animate={{ opacity: 1, x: 0 }}
         transition={{ duration: 1 }}
@@ -13,8 +13,33 @@ export default function TransitionType() {
           {" "}
           We love Animation. We are enjoying it a lot. Framer motion is great
         </p>
+      </motion.div> */}
+
+      <motion.div
+        initial={{ opacity: 0, x: "-100vh" }}
+        animate={{ opacity: 1, x: 0 }}
+        transition={{ duration: 2 , type:"spring" , bounce:0.8 }}
+      >
+        <h1>Learning Animation</h1>
+
+        <p>
+          {" "}
+          We love Animation. We are enjoying it a lot. Framer motion is great
+        </p>
       </motion.div>
 
+      {/* <motion.div
+        initial={{ opacity: 0, x: "-100vh" }}
+        animate={{ opacity: 1, x: 0 }}
+        transition={{ duration: 1 , type:"inertia" }}
+      >
+        <h1>Learning Animation</h1>
+
+        <p>
+          {" "}
+          We love Animation. We are enjoying it a lot. Framer motion is great
+        </p>
+      </motion.div> */}
     </>
   );
 }
